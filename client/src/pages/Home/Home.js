de895985ca8e4f9adb8ab1442bfba66124945567
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-07-15 09:29:16 +0200

    Finish with header component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Home.css';
 import Navbar from '../../Components/navbar/Navbar';
+import Header from '../../Components/Header/Header';
 
 const Home = () => {
   return (
-    <div><Navbar /></div>
+    <div>
+      <Navbar />
+      <Header />
+    </div>
   )
 }
 
