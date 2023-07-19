39632d68fe61898a2fa098d132740a61b347a046
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-07-19 20:16:14 +0200

    Make verification token and user apis

--- server/Routers/Users.js
+++ server/Routers/Users.js
@@ -1,10 +1,15 @@
 import express from "express";
+import { verifyToken, verifyUser } from "../Utils/VerifyToken.js";
 
 const router = express.Router();
 
 
-router.get("/",(req,res)=>{
-    res.send("Hello from users routes")
+router.get("/checkauth",verifyToken,(req,res,next)=>{
+    res.send("hello user, you are logged in")
+})
+
+router.get("/checkuser/:id",verifyUser, (req,res,next)=>{
+    res.send("hello user, you are logged in and you can delete your account")
 })
 
 export default router
