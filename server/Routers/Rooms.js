eb1fb2bd11a72019a8c43f3a080c6db1dba41c86
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-07-21 08:53:26 +0200

    Make CRUD with model relations

--- server/Routers/Rooms.js
+++ server/Routers/Rooms.js
@@ -1,10 +1,99 @@
 import express from "express";
+import Room from '../Models/Room.js';
+import Hotel from '../Models/Hotel.js'
+import { verifyAdmin } from "../Utils/VerifyToken.js";
 
 const router = express.Router();
 
 
-router.get("/",(req,res)=>{
-    res.send("Hello from rooms routes")
-})
+router.post("/:hotelid",verifyAdmin, async (req, res,next) => {
+  const hotelId = req.params.hotelid;
+  const newRoom = new Room(req.body);
+
+  try {
+    const savedRoom = await newRoom.save();
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
+  });
+
+
+
+ 
+
+
+
+  router.put("/:id",verifyAdmin, async (req, res) => {
+    try {
+      const updatedRoom = await Room.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updatedRoom);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
+
+
+
+
+  router.delete("/:id/:hotelid",verifyAdmin, async (req, res) => {
+    const hotelId = req.params.hotelid;
+    try {
+      await Room.findByIdAndDelete(req.params.id);
+      try {
+        await Hotel.findByIdAndUpdate(hotelId, {
+          $pull: { rooms: req.params.id },
+        });
+      } catch (err) {
+        next(err);
+      }
+      res.status(200).json("Room has been deleted.");
+    } catch (err) {
+      next(err);
+    }
+  });
+
+
+
+
+
+  router.get("/:id", async (req, res) => {
+    try {
+      const room = await Room.findById(
+        req.params.id
+      );
+      res.status(200).json(room);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
+
+  router.get("/", async (req, res) => {
+    try {
+      const rooms = await Room.find();
+      res.status(200).json(rooms);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
 
 export default router
